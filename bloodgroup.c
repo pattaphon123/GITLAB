ebe2f2041be0512ffe48438208d0e3d4c6927263
sakilaant <sakilasaiiam@gmail.com>
2023-09-21 00:18:49 +0700

    Modified second bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your Pattaphon? ");
+    printf("What is your Pattaphon? TTTT");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O+++]? ");
+      printf("What is your blood group TTTT [A, B, AB, or O+++]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
